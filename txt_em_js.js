5dad92b08fa40ffb1db1c18206a746867aa02ba9
AgIgor <agigor@outlook.com>
2021-10-17 15:19:21 -0300

    txt em json

---
+++ txt_em_js.js
@@ -0,0 +1,17 @@
+ let lista_nomes = document.getElementById("nomes").innerText.split(" ")
+ console.log(lista_nomes)
+
+ let lista_sobrenomes = document.getElementById("sobrenomes").innerText.split(",")
+ console.log(lista_sobrenomes)
+
+
+ for(let i = 0;i< lista_nomes.length;i++){
+     let nome = ('"'+lista_nomes[i]+'", ') 
+     document.getElementById("saidaNomes").innerHTML += nome
+ }
+
+ for(let i = 0;i< lista_sobrenomes.length;i++){
+    let sobrenome = ('"'+lista_sobrenomes[i]+',') 
+    document.getElementById("saidaSobrenomes").innerHTML += sobrenome
+}
+
